2f32b93b222abd3f973f898aa2317e009ab92a82
Syed Muhammad Wahaj Hassan <wahajhassan007@gmail.com>
2025-04-10 15:26:15 +0500

    Intial Commit

--- app/src/main/java/com/example/pixelcodex/MainActivity.java
+++ app/src/main/java/com/example/pixelcodex/MainActivity.java
@@ -1,9 +1,8 @@
 package com.example.pixelcodex;
 
-import android.content.Intent;
 import android.os.Bundle;
-import android.widget.TextView;
 
+import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
@@ -11,27 +10,15 @@ import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
 
-    private Compiler EdgeToEdge;
-
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable();
+        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
-        });
-
-        TextView signupText = findViewById(R.id.signUpTextView);
-
-        // Set click listener to navigate to SignupActivity
-        signupText.setOnClickListener(v -> {
-            Intent intent = new Intent(MainActivity.this, signup.class);
-            startActivity(intent);
         });
     }
 }
